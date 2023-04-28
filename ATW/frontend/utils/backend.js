2273fabe4185fb770458ef07b7daa6fb61899d7d
ebmiller510 <llhappimess@gmail.com>
2023-04-28 09:33:38 -0700

    updated JWT with axios requests and the sign-up log-in forms

--- ATW/frontend/utils/backend.js
+++ ATW/frontend/utils/backend.js
@@ -1,46 +1,64 @@
 // store axios requests to the backend
-
 import axios from 'axios'
+const authHeader = { headers: { 'Authorization': localStorage.getItem('userToken') } }
+
+// ---------- USERS ----------
+//------------------------------
+export async function signUp(user) {
+    const { data } = await axios.post('/api/users/signup', user)
+    return data
+}
+
+export async function logIn(user) {
+    const { data } = await axios.post('/api/users/login', user)
+    console.log({data})
+    return data
+}
+
+
 
 // ---------- LANDMARKS ----------
 //------------------------------
-export async function getLandmarks(countryName){
+export async function getLandmarks( countryName){
+    // getting all landmarks for whatever country is passed in
     const response = await axios.get(`/api/landmarks/country/${countryName}`)
     return response.data
 }
 
 export async function postLandmark(landmark){
-    const response = await axios.post('/api/landmarks/', landmark)
+    const response = await axios.post('/api/landmarks/', landmark, authHeader)
     return response.data
 }
 
 export async function updateLandmark(landmark, id){
-    const response = await axios.put(`/api/landmarks/${id}`, landmark)
+    const response = await axios.put(`/api/landmarks/${id}`, landmark, authHeader)
     return response.data
 }
 export async function deleteLandmark(id){
-    const response = await axios.delete(`/api/landmarks/${id}`)
+    const response = await axios.delete(`/api/landmarks/${id}`, authHeader)
     return response.data
 }
 
 // ---------- COMMENTS ----------
 //------------------------------
 export async function getComments(countryName){
+    // get all comments for whatever country is passed in
     const response = await axios.get(`/api/comments/country/${countryName}`)
-    return console.log(response.data + "this is the response")
+    return response.data
 }
 
 export async function postComment(comment){
-    const response = await axios.post('/api/comments/', comment)
+    // first arg is the endpoint and the second arg is req body
+    const response = await axios.post('/api/comments/', comment, authHeader)
     return response.data
 }
 
 export async function updateComment(comment, id){
-    const response = await axios.put(`/api/comments/${id}`, comment)
+    const response = await axios.put(`/api/comments/${id}`, comment, authHeader)
     return response.data
 }
 
 export async function deleteComment(id){
-    const response = await axios.delete(`/api/comments/${id}`)
+    const response = await axios.delete(`/api/comments/${id}`, authHeader)
     return response.data
 }
